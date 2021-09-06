82d2319750b4284d6e91c92d1d340b28c3f735a0
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-06 16:02:39 +0300

    This will debug the user tests

--- test.py
+++ test.py
@@ -1,33 +1,43 @@
+from credentials import User_Credentials
 import unittest # importing the unittest module
 from user import User
+from credentials import User_Credentials
 
 class testUsers(unittest.TestCase):
     def setUp(self):
         """
         This method runs before any test
         """
-        self.newUser = User("daniel", "12345678")
+        self.credentialsList = User_Credentials("facebook", "daniel", "12345678")
     def tearDown(self):
-        User.userList = []
+        User_Credentials.credentialsList = []
 
     def test_init(self):
         """
         test case to check if object or class is instanciated properly 
         """
-        self.assertEqual(self.newUser.username, 'daniel')
-        self.assertEqual(self.newUser.password, '12345678')
+        self.assertEqual(self.credentialsList.userAccount, 'facebook')
+        self.assertEqual(self.credentialsList.username, 'daniel')
+        self.assertEqual(self.credentialsList.password, '12345678')
 
     def test_saveUser(self):
-        self.newUser.saveUser()
-        self.assertEqual(len(User.userList),1)
+
+        """
+        test case to check if objects are saved properly 
+        """ 
+        self.credentialsList.saveCredentials()
+        self.assertEqual(len(User_Credentials.credentialsList),1)
 
     def test_deleteUser(self):
-        self.newUser.saveUser()
-        defaultUser= User("Daniel-Muruthi", "12345678")
-        defaultUser.saveUser()
 
-        self.newUser.deleteUser()
-        self.assertEqual(len(User.userList),1)
+        """
+        test case to check if credentials are deleted properly
+        """
+        self.credentialsList.saveCredentials()
+        test_multi_credentials = User_Credentials("twitter", "el_asad_the_don ", " 1234")
+        test_multi_credentials.saveCredentials()
+        self.credentialsList.deleteCredentials()
+        self.assertEqual(len(User.userList),0)
 
 if __name__ == "__main__":
     unittest.main()
